abe49c27b0065e603829591ce0baf830d7b5d613
Vijay Reddy <vijayreddy.vanga8@gmail.com>
2024-08-30 21:05:34 +0530

    Made a few UI changes for improved responsiveness

---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+// Updating courses in popular courses
+let navLinks = document.querySelectorAll(".popular-courses-navbar button");
+
+function popCoursesHandler(event) {
+  Array.from(event.target.parentElement.children).forEach((element) => {
+    if (element === event.target) {
+      element.classList.add("btn-primary");
+      element.classList.remove("btn-outline-light");
+    } else {
+      element.classList.remove("btn-primary");
+      element.classList.add("btn-outline-light");
+    }
+  });
+
+  if (
+    event.target.innerText === "Front-end development" ||
+    event.target.value === "front-end"
+  ) {
+    document.querySelectorAll(".course-details-card").forEach((e) => {
+      if (e.classList.contains("fe")) {
+        e.classList.remove("d-none");
+        e.classList.add("d-block");
+      } else {
+        e.classList.add("d-none");
+        e.classList.remove("d-block");
+      }
+    });
+  } else if (
+    event.target.innerText === "Fullstack development" ||
+    event.target.value === "full-stack"
+  ) {
+    document.querySelectorAll(".course-details-card").forEach((e) => {
+      if (e.classList.contains("full")) {
+        e.classList.remove("d-none");
+        e.classList.add("d-block");
+      } else {
+        e.classList.add("d-none");
+        e.classList.remove("d-block");
+      }
+    });
+  } else if (
+    event.target.innerText === "Testing" ||
+    event.target.value === "testing"
+  ) {
+    document.querySelectorAll(".course-details-card").forEach((e) => {
+      if (e.classList.contains("test")) {
+        e.classList.remove("d-none");
+        e.classList.add("d-block");
+      } else {
+        e.classList.add("d-none");
+        e.classList.remove("d-block");
+      }
+    });
+  } else if (
+    event.target.innerText === "UI/UX designing" ||
+    event.target.value === "ui/ux"
+  ) {
+    document.querySelectorAll(".course-details-card").forEach((e) => {
+      if (e.classList.contains("ui")) {
+        e.classList.remove("d-none");
+        e.classList.add("d-block");
+      } else {
+        e.classList.add("d-none");
+        e.classList.remove("d-block");
+      }
+    });
+  } else {
+    document.querySelectorAll(".course-details-card").forEach((e) => {
+      e.classList.remove("d-none");
+      e.classList.add("d-block");
+    });
+  }
+}
+
+navLinks.forEach((element) => {
+  element.addEventListener("click", (event) => popCoursesHandler(event));
+});
+
+document
+  .querySelector(".popular-courses-dropdown")
+  .addEventListener("change", (event) => popCoursesHandler(event));
